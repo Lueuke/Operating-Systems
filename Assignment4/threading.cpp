d2216dc6f7fa6013f467537106f2c337676f648a
Luke <lukedekan@gmail.com>
2024-03-01 12:42:36 -0600

    Threading Start

--- Assignment4/threading.cpp
+++ Assignment4/threading.cpp
@@ -1,5 +1,12 @@
 int main(int argc, char const *argv[])
 {
-    
+    if (argc == 2 )
+    {
+        // No File input 
+    }
+    else if ( argc > 2)
+    {
+        // Wrong file name 
+    }
     return 0;
 }
