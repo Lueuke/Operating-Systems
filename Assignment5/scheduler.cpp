387b2bcc6ab587c42cfaa703a7266e86a6cad844
Luke <lukedekan@gmail.com>
2024-03-23 19:04:35 -0500

    Scheduler Start

---
+++ Assignment5/scheduler.cpp
@@ -0,0 +1,17 @@
+
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+    // Check for Input File
+    if (argc != 2) {
+        cout << "ERROR No input file" << argv[0] << endl;
+        return 1;
+    }
+
+   
+    return 0;
+}
