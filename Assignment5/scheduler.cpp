1ae6561c0fb2a5b2ea061cd7989e0db7e66e42d4
Luke Dekan <lukedekan@gmail.com>
2024-03-25 14:55:58 -0500

    Updated Argument Parsing

--- Assignment5/scheduler.cpp
+++ Assignment5/scheduler.cpp
@@ -19,17 +19,20 @@ void StoreNumbers()
 
 int main(int argc, char *argv[])
 {
-    // Check for Input File
-    if (argc != 2) {
-        cout << "ERROR No input file" << argv[0] << endl;
-        return 1;
-    }
+    
 
     string Inputfile = argv[1];
     
     ifstream Input(Inputfile);
 
+    // Check for Input File
+    if (!Input) {
+        cout << "ERROR No input file" << endl;
+        return 1;
+    }
+
     string line;
+
 
     int NumberOfProcess = 0;  
 
