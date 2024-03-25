80ca517b3f8360555045786e088379a203791c09
Luke <lukedekan@gmail.com>
2024-03-25 09:40:38 -0500

    Need to Store Numbers into Struct

--- Assignment5/scheduler.cpp
+++ Assignment5/scheduler.cpp
@@ -1,15 +1,20 @@
 
 #include <iostream>
 #include <fstream>
+#include <string>
 
 using namespace std;
 
 struct Time {
-    string name; 
+    string names; 
     int ArriveTime;
     int ServiceTime;
 };
 
+void StoreNumbers()
+{
+
+}
 
 
 int main(int argc, char *argv[])
@@ -19,6 +24,23 @@ int main(int argc, char *argv[])
         cout << "ERROR No input file" << argv[0] << endl;
         return 1;
     }
+
+    string Inputfile = argv[1];
+    
+    ifstream Input(Inputfile);
+
+    string line;
+
+    int lineCount = 0;  
+
+    while (getline(Input,line)) 
+    {  // Loop through each line in the file
+      lineCount++;  // Incrementing line count for each line read
+    }
+
+
+   // Time proccessList[lineCount];
+
 
    
     return 0;
