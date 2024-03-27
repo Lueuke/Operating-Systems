67e68aed04d4821ec239383606645d078f21993f
Luke Dekan <lukedekan@gmail.com>
2024-03-26 21:42:39 -0500

    Round Robbin Added

--- Assignment5/scheduler.cpp
+++ Assignment5/scheduler.cpp
@@ -49,6 +49,47 @@ void FCFSScheduler(ifstream& inputFile)
 
 }
 
+
+void RoundRobinScheduler(ifstream& inputFile,int timeQuantum)
+{
+    // Round Robin Scheduler
+    ofstream RROutput10("rr10.out");
+    
+    queue<ProcessName> ProcessQueue;
+
+    string name;
+    int arrivalTime;
+    int serviceTime;
+
+    while (inputFile >> name >> arrivalTime >> serviceTime) 
+    {
+        ProcessName process = {name, arrivalTime, serviceTime};
+        ProcessQueue.push(process);
+    }
+
+    while (!ProcessQueue.empty()) 
+    {
+        ProcessName process = ProcessQueue.front();
+        ProcessQueue.pop();
+
+        if (process.ServiceTime > timeQuantum)
+        {
+            for (int i = 0; i < timeQuantum; i += timeQuantum) 
+            {
+                RROutput10 << process.name << endl;
+            }
+            ProcessQueue.push(process);
+        }
+        else
+        {
+            for (int i = 0; i < process.ServiceTime; i += timeQuantum) 
+            {
+                RROutput10 << process.name << endl;
+            }
+        }
+    }
+}
+
 int main()
 {
 
@@ -61,7 +102,9 @@ int main()
         return 1;
     }
 
-    FCFSScheduler(Input);
+    RoundRobinScheduler(Input,10);
+    //FCFSScheduler(Input);
+    
 
     return 0;
 }
