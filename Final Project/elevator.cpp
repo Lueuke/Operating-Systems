5ddf365bdd97cd58d86e406daad2f0553c5b9892
Luke <lukedekan@gmail.com>
2024-04-15 21:08:41 -0500

    Added Command Line Args Need scheduling Algorithm

--- Final Project/elevator.cpp
+++ Final Project/elevator.cpp
@@ -4,8 +4,27 @@
 #include <thread>
 #include <mutex>
 #include <curl/curl.h>
-
+#include <fstream>
+#include <sstream>
 using namespace std;
+
+struct Person {
+    string Name;
+    int startFloor;
+    int endFloor;
+    int timeTick;
+};
+
+struct Elevator 
+{
+    string Name;
+    int lowestFloor;
+    int highestFloor;
+    int currentFloor;
+    int totalCapacity;
+};
+
+queue<Person> peopleQueue;
 
 static size_t WriteCallback(void *contents, size_t size, size_t nmemb, void *userp)
 {
@@ -21,6 +40,22 @@ void SimStart()
     CURL* curl = curl_easy_init();
 
     curl_easy_setopt(curl, CURLOPT_URL, "http://localhost:5432/Simulation/start");
+    curl_easy_setopt(curl, CURLOPT_CUSTOMREQUEST, "PUT");
+
+    CURLcode res = curl_easy_perform(curl);
+
+    curl_easy_cleanup(curl);
+    curl_global_cleanup();
+}
+
+void SimStop()
+{
+    
+    curl_global_init(CURL_GLOBAL_ALL);
+
+    CURL* curl = curl_easy_init();
+
+    curl_easy_setopt(curl, CURLOPT_URL, "http://localhost:5432/Simulation/stop");
     curl_easy_setopt(curl, CURLOPT_CUSTOMREQUEST, "PUT");
 
     CURLcode res = curl_easy_perform(curl);
@@ -50,7 +85,15 @@ void InputThread()
         {
             cerr << "curl_easy_perform() failed: " << curl_easy_strerror(res);
         }
+        else {
+                stringstream ss(readBuffer);
+                string Name;
+                int startFloor, endFloor, timeTick;
+                ss >> Name >> startFloor >> endFloor >> timeTick;
+                peopleQueue.push({Name, startFloor, endFloor, timeTick});
+        }
 
+        readBuffer.clear();
         this_thread::sleep_for(chrono::seconds(1));
     }
     
@@ -70,6 +113,17 @@ void SchedulerThread()
 
 int main(int argc, char* argv[]) 
 {
+
+    
+    ifstream ElevatorFile(argv[1]);
+
+    // Check if input file exists
+    if (!ElevatorFile) 
+    {
+        cout << "ERROR No input file" << endl;
+        return 1;
+    }
+
     SimStart();
 
     thread inputThread(InputThread);
