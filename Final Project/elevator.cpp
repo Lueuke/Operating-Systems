5b42b1ad07c720026443e10183808b9f9ce8d337
Luke <lukedekan@gmail.com>
2024-04-16 22:34:37 -0500

    Figured out Output Stream Scheduler next

--- Final Project/elevator.cpp
+++ Final Project/elevator.cpp
@@ -5,11 +5,14 @@
 #include <mutex>
 #include <curl/curl.h>
 #include <fstream>
+#include <vector>
 #include <sstream>
+#include <algorithm>
 using namespace std;
 
 struct Person {
     string Name;
+    int id;
     int startFloor;
     int endFloor;
     int timeTick;
@@ -25,6 +28,8 @@ struct Elevator
 };
 
 queue<Person> peopleQueue;
+vector<Elevator> elevators; 
+
 
 static size_t WriteCallback(void *contents, size_t size, size_t nmemb, void *userp)
 {
@@ -88,8 +93,8 @@ void InputThread()
         else {
                 stringstream ss(readBuffer);
                 string Name;
-                int startFloor, endFloor, timeTick;
-                ss >> Name >> startFloor >> endFloor >> timeTick;
+                int id, startFloor, endFloor, timeTick;
+                ss >> id >> Name >> startFloor >> endFloor >> timeTick;
                 peopleQueue.push({Name, startFloor, endFloor, timeTick});
         }
 
@@ -103,7 +108,39 @@ void InputThread()
 
 void OutputThread()
 {
-    
+    curl_global_init(CURL_GLOBAL_ALL);
+    CURL* curl = curl_easy_init();
+    string readBuffer;
+
+    while(true)
+    {
+        if (!peopleQueue.empty())
+        {
+            Person person = peopleQueue.front();
+            peopleQueue.pop();
+            
+            auto assignedElevator = find_if(elevators.begin(), elevators.end(), [&](Elevator a) {
+                return a.currentFloor == person.startFloor;
+            });
+
+             string putUrl = "http://localhost:5432/AddPersonToElevator/" + to_string(person.id) + "/" + assignedElevator->Name;
+
+            curl_easy_setopt(curl, CURLOPT_URL, "http://localhost:5432/NextOutput");
+            curl_easy_setopt(curl, CURLOPT_CUSTOMREQUEST, "PUT");
+
+            string data = person.Name + " " + to_string(person.startFloor) + " " + to_string(person.endFloor) + " " + to_string(person.timeTick);
+            curl_easy_setopt(curl, CURLOPT_POSTFIELDS, data.c_str());
+
+            CURLcode res = curl_easy_perform(curl);
+
+            if (res != CURLE_OK) 
+            {
+                cerr << "curl_easy_perform() failed: " << curl_easy_strerror(res);
+            }
+        }
+        this_thread::sleep_for(chrono::seconds(1));
+    }
+
 }
 
 void SchedulerThread()
@@ -122,6 +159,24 @@ int main(int argc, char* argv[])
     {
         cout << "ERROR No input file" << endl;
         return 1;
+    }
+
+    string line;
+    while (getline(ElevatorFile, line)) 
+    {
+        stringstream ss(line);
+        Elevator elevator;
+
+        getline(ss, elevator.Name, '\t');
+        ss >> elevator.lowestFloor;
+        ss.ignore();
+        ss >> elevator.highestFloor;
+        ss.ignore();
+        ss >> elevator.currentFloor;
+        ss.ignore();
+        ss >> elevator.totalCapacity;
+
+        elevators.push_back(elevator);
     }
 
     SimStart();
